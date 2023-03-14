a8d70d6e2d7f9bf6341bb1009a1eb13786872356
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-03-14 14:11:40 -0500

    Was not returning from bad category id, server erroring out

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,6 +20,7 @@ router.get('/:id', async (req, res) => {
     if (!catData) {
       // does a product exist with that id
       res.status(404).json({ message: "No category found with that id!" });
+      return;
     }
     res.status(200).json(catData);
   } catch (err) {
